e76f2c2bf9a38865fcfcb7fdafa436d340e681e4
Ömer Genç <omergnc1@gmail.com>
2022-02-01 18:37:59 +0300

    Add | template engine

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
 
 const app = express();
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+// MIDDLEWARES
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
+  res.render("index");
+});
 
-  res.status(200).send(blog);
+app.get("/about-me", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact-me", (req, res) => {
+  res.render("contact");
 });
 
 const port = 3000;
